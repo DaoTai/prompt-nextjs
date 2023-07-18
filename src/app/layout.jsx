a9d9b7b19ca7780009504803084e220d68783c0f
Dao Tai <ductaidao24120001@gmail.com>
2023-07-18 14:56:17 +0700

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,15 +13,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Provider>
-          <div className="main">
-            <div className="gradient" />
-          </div>
-          <main className="app">
-            <Nav />
-            {children}
-          </main>
-        </Provider>
+        {/* <Provider> */}
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
+        {/* </Provider> */}
       </body>
     </html>
   );
